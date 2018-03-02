36415afbdae40d60a34e8a800ca86da8d8f5e44c
mingyard <mingyard@163.com>
2018-03-02 18:22:42 +0800

    修改配置文件

--- deviceConfig.js
+++ deviceConfig.js
@@ -1,39 +1,39 @@
 var CONFIG = {
-	//屏幕配置
-	"screen":{
-		"minX": 400,
-		"maxX":1240,
-		"minY":860,
-		"maxY":1560,
-		"leftX":176
-	},
-	//领种子点
-	"getGlodPoint" : {
-		X: 770,
-		Y: 2200
-	},
-	//关闭广告点
-	"closeAd1" : {
-		X:1370,
-		Y:450
-	},
-	"closeAd2": {
-		X:1350,
-		Y:880
-	},
-	//关闭确认页
-	"closePoint" :{
-		X:1350,
-		Y:528
-	},
-	//滑块点击位置
-	"slidePoint" : {
-		X:280,
-		Y:1700
-	},
-	//滑块速度
-	"slideTime" : 6000,
-	"projectPath" : "/Users/ming/yard/cashBookApi"
+    //屏幕配置
+    "screen":{
+        "minX": 400,
+        "maxX":1240,
+        "minY":860,
+        "maxY":1560,
+        "leftX":176
+    },
+    //领种子点
+    "getGlodPoint" : {
+        X: 770,
+        Y: 2200
+    },
+    //关闭广告点
+    "closeAd1" : {
+        X:1370,
+        Y:450
+    },
+    "closeAd2": {
+        X:1350,
+        Y:880
+    },
+    //关闭确认页
+    "closePoint" :{
+        X:1350,
+        Y:528
+    },
+    //滑块点击位置
+    "slidePoint" : {
+        X:280,
+        Y:1700
+    },
+    //滑块速度
+    "slideTime" : 7000,
+    "projectPath" : "/Users/ming/yard/cashBookApi"
 };
 
 module.exports = CONFIG;

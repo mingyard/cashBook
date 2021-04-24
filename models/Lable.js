2ab9610c7e8b5c195e3fb653ec346881f38f39db
mingyard <mingyard@163.com>
2021-04-24 16:12:07 +0800

    提交本地代码

--- models/Lable.js
+++ models/Lable.js
@@ -6,6 +6,8 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const timestamps = require('mongoose-timestamp')
 
+//消费类型表
+
 const LableSchema = new Schema(
     {
         icon:         String,     //图标id
--- routes/api.js
+++ routes/api.js
@@ -12,12 +12,15 @@ api.get('/share/info', shareInfo.getNowShareInfo)
 
 //登陆
 api.post('/login', login.login)
+
 //获取用户记账本列表
 api.post('/cash/list', middleware.getUserId, cash.cashList)
 //创建记账本
 api.post('/cash/create', middleware.getUserId, cash.crateCash)
 //退出账本
 api.post('/cash/del', middleware.getUserId, cash.checkCash(), cash.del)
+
+
 //添加账单
 api.post("/bill/add", middleware.getUserId, cash.checkCash(), bill.add)
 //添加账单类型
@@ -28,11 +31,15 @@ api.post("/bill/typeList", middleware.getUserId, cash.checkCash(), bill.typeList
 api.post("/bill/delType", middleware.getUserId, bill.checkLable, bill.delType)
 //修改账单类型
 api.post("/bill/updateType", middleware.getUserId, bill.checkLable, bill.updateType)
+
+
 //获取最后修改账本信息
 api.post("/cash/info", middleware.getUserId, cash.checkCash(false),cash.lastCashInfo)
 //获取账本类型列表
 api.post("/cashType/list", middleware.getUserId, cash.getTypeList)
 //获取指定类型账本数量
 api.post("/cashType/count", middleware.getUserId, cash.typeCount)
+
+
 //上传图片接口
 api.post("/upload/image", multer().single('file'), cash.uploadImage)
